7fbfbfb7cad25dd91fc157c99d740ada4d7125ad
luolihua <luolihua@beadwallet.com>
2019-06-30 11:56:52 +0800

    begin sleuth

--- user/user-server/src/main/java/com/vic/utils/CookieUtils.java
+++ user/user-server/src/main/java/com/vic/utils/CookieUtils.java
@@ -1,18 +1,12 @@
 package com.vic.utils;
 
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import com.vic.vo.ResultVO;
 
 public class CookieUtils {
 
-    public static void set(HttpServletResponse response,
-                           String name, String value,
-                           int maxAge) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAge);
-        response.addCookie(cookie);
+    public static ResultVO success(Object object) {
+        return null;
     }
 
 }
