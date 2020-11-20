d5c8dab4d0e2d25f8ca6a1182ad57bf1dbb0b8f1
罗利华 <luolihua@beadwallet.com>
2020-11-20 11:59:03 +0800

    uniform config

--- order/order-server/src/main/java/com/vic/orderserver/OrderServerApplication.java
+++ order/order-server/src/main/java/com/vic/orderserver/OrderServerApplication.java
@@ -2,12 +2,14 @@ package com.vic.orderserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {"com.vic.feign.product"})
+@EnableFeignClients(basePackages = {"com.vic.feign"})
+@EntityScan("com.vic")
 public class OrderServerApplication {
 
     public static void main(String[] args) {
