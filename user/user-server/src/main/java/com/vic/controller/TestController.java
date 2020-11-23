681afa9e1c8534be52be5637efac536900c76722
罗利华 <luolihua@beadwallet.com>
2020-11-23 15:29:06 +0800

    admin add login/logout, print real time log for user

---
+++ user/user-server/src/main/java/com/vic/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.vic.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 罗利华
+ * date: 2020/11/23 13:55
+ */
+@Slf4j
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @RequestMapping("test1")
+    public String test1() {
+        for(int i = 0; i < 100; i++) {
+            log.info("print log {}", i);
+        }
+        return "ok";
+    }
+
+}
