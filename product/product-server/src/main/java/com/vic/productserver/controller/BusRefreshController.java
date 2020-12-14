572e2e6b6dbf451fb6566f5349cd73f95cb3c64b
罗利华 <luolihua@beadwallet.com>
2020-12-14 16:30:12 +0800

    add bus-refresh controller

---
+++ product/product-server/src/main/java/com/vic/productserver/controller/BusRefreshController.java
@@ -0,0 +1,27 @@
+package com.vic.productserver.controller;
+
+import com.vic.config.BusRefreshConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试是否取到git上的配置文件
+ */
+@RestController
+@Slf4j
+@RequestMapping("busRefresh")
+//@RefreshScope
+public class BusRefreshController {
+
+    @Autowired
+    private BusRefreshConfig config;
+
+    @GetMapping("config")
+    public BusRefreshConfig config() {
+        return config;
+    }
+
+}
